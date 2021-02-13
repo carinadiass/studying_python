a4df7503edf099be875c9ff3ff55dfffaa2a8e2a
carinadiass <carinadiass@gmail.com>
2021-02-13 13:20:42 -0300

    Minha primeira aula de python

---
+++ Aulas Básico Python/aula1 - comentario.py
@@ -0,0 +1,15 @@
+# Comentario -- PEP8
+print ( 'Linha 1' )  # Padrão comentário dois espaços laterais  e um no texto
+print ( 'Linha 2' )
+print ( 'Linha 3' )
+print ( 'Linha 4' )
+"""
+print ( 'Linha 5' )
+print ( 'Linha 6' )
+
+"""
+
+'''
+Comentário
+
+'''
