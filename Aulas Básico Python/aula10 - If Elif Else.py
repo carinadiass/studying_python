54a283569be3279db744d9b8a2285e0281ab0c6f
carinadiass <carinadiass@gmail.com>
2021-02-27 10:46:28 -0300

    Aula10 if elif e else

---
+++ Aulas Básico Python/aula10 - If Elif Else.py
@@ -0,0 +1,13 @@
+"""
+Condições if elif e else
+
+"""
+
+if False:
+    print('Verdadeira!')
+elif False:
+    print("Agora é verdadeiro")
+elif False:
+    print("Sempre verdadeiro")
+else:
+    print('Não é verdadeira!')
