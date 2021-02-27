2107818f28ba86f790bc68a6c752962cbbf3bd45
carinadiass <carinadiass@gmail.com>
2021-02-27 10:22:37 -0300

    Aula9 input de dados

---
+++ Aulas Básico Python/aula9 -  input de dados.py
@@ -0,0 +1,23 @@
+"""
+Entrada de Dados
+Sempre retorna uma String, independente do valor que seja digitado!
+
+"""
+
+#nome = input("Qual o seu nome?\n")
+##idade = input("Qual sua idade?\n")
+
+###ano_nascimento = 2021 - int(idade)
+
+#print(f"Usuário digitou {nome} e o tipo da variável {type(nome)}")
+#print(f"Tem {idade} anos")
+##print(f"Nasceu no ano {ano_nascimento}")
+
+"""
+CALCULADORA DE SOMA
+
+"""
+numero_1 = int(input("Digite um número "))
+numero_2 = int(input("Digite outro número "))
+
+print(numero_1 + numero_2)
